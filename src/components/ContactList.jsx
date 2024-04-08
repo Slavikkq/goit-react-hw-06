5a24d8a37024c3e3b2272e3f17a7ca639485fc7e
Slavikkq <yaroslavmanuljak@gmail.com>
2024-04-08 23:23:17 +0300

    react hw 06

--- src/components/ContactList.jsx
+++ src/components/ContactList.jsx
@@ -2,8 +2,8 @@ import { useSelector } from "react-redux";
 import { selectContacts } from "../redux/contactsSlice";
 import Contact from "./Contact";
 
-export default function ContactList() {
-  const contacts = useSelector(selectContacts);
+export default function ContactList({ filter }) {
+  const contacts = useSelector((state) => selectContacts(state, filter));
 
   if (!Array.isArray(contacts)) {
     return <p>No contacts available.</p>;
